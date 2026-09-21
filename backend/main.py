b986a68b7e1bbc95795e92c309a72e48de43f36b
Ryan Lewis <lewisryan2310@gmail.com>
2026-09-21 15:10:36 +0530

    Allow deployed frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -46,9 +46,10 @@ app = FastAPI(title="OrderNest API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://ordernest-4kyk.onrender.com"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
